7250202983e2477dfd990ce44375243eed063755
BSaranya2000 <bsaranya047@gmail.com>
2023-07-24 05:24:26 -0700

    second commit

--- src/counter/parent.jsx
+++ src/counter/parent.jsx
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import Child from "./child";
 const Parent= () => {
     const [count,setcount]=useState(0);
-    console.log(count,"value");
     return(
             <div>
                 <Child count={count} addcount={()=>setcount(count+2)}
